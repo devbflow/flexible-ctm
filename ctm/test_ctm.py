baf42ee54157cc256da2995aa2420f6dde2582ce
Florian Biebel <f.biebeldev@gmail.com>
2021-10-13 12:34:51 +0200

    added test for get_conv_params

--- ctm/test_ctm.py
+++ ctm/test_ctm.py
@@ -1,6 +1,16 @@
 import unittest
 
+from . import ctm
+
 
 def test_get_conv_params():
-    
-    return
+    in_dims = [14]
+    out_dims = [12]
+    correct_p = [{'kernel_size': 3, 'stride': 1, 'padding': 0, 'dilation': 1}]
+
+    for i, o, cp in zip(in_dims, out_dims, correct_p):
+        p = ctm.get_conv_params(i, o)
+        assert cp == p
+    print("No assertionError found, test_get_conv_params OK!")
+
+
