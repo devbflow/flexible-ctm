1797ccf9fc57520547097740bb77215ed76c0a99
Florian Biebel <f.biebeldev@gmail.com>
2021-10-15 17:07:50 +0200

    added some model-related code to main script

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ It serves as guideline, how to use the module in your own pipeline, if imported.
 """
 
 import torch
+import torch.nn as nn
 import yaml
+from pathlib import Path
 
-from .ctm.ctm import CTM
+from ctm.ctm import CTM
+from preprocessing.prepr_model import preprocess_backbone
 
+
+### CONSTANTS ###
 # config filename, facilitates usage with different configs
 CONFIG_FILENAME = 'config.yml'
+
+MODELS_PATH = Path('./models/')
+DATA_PATH = Path('./data/')
 
 # load config
 with open(CONFIG_FILENAME, 'r') as cfile:
@@ -35,21 +43,37 @@ dataset_cfg = cfg['dataset']
 
 ### MODEL CONFIGS ###
 model_cfg = cfg['model']
+backbone_out_dim = None # same as default value
 
+# in case one disables parts, they will be omitted/be Identity modules
+backbone = nn.Identity()
+ctm = nn.Identity()
+metric = nn.Identity()
+
+## BACKBONE ##
 if model_cfg['parts']['backbone']:
     backbone_cfg = model_cfg['backbone']
-
+    model_file = 'backbone_{}.pth'.format(backbone_cfg['name'])
+    try:
+        # load model
+        torch.load(MODELS_PATH / model_file)
+        #TODO
+    except FileNotFoundError:
+        raise FileNotFoundError("model '{}' does not exist!".format(MODELS_PATH / model_file))
     #TODO: init backbone
     backbone_out_dim = 14 #PLACEHOLDER
 
+## CTM ##
 if model_cfg['parts']['ctm']:
     ctm_cfg = model_cfg['ctm']
     ctm = CTM(ctm_cfg, dataset_cfg, backbone_out_dim)
     #TODO: init ctm
 
+## METRIC ##
 if model_cfg['parts']['metric']:
     metric_cfg = model_cfg['metric']
     #TODO: init metric
+
 
 ### TRAIN/TEST/VAL MODE CONFIG ###
 MODE = cfg['mode'] # train, test, val modes
--- preprocessing/dl_backbone.py
+++ preprocessing/dl_backbone.py
@@ -1,12 +1,16 @@
-import torchvision.models as models
 import torch
+from torchvision import models
 
 import os
 from pathlib import Path
 
 PATH = Path("../models")
 
-resnets = {'resnet18': models.resnet18, 'resnet34': models.resnet34, 'resnet50': models.resnet50, 'resnet101': models.resnet101, 'resnet152': models.resnet152}
+resnets = {'resnet18': models.resnet18,
+           'resnet34': models.resnet34,
+           'resnet50': models.resnet50,
+           'resnet101': models.resnet101,
+           'resnet152': models.resnet152}
 
 
 if __name__ == "__main__":
