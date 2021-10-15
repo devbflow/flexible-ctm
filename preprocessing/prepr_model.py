f5a53b314a77818b023a0753d8d6cd807050c622
Florian Biebel <f.biebeldev@gmail.com>
2021-10-15 16:17:25 +0200

    fixed references in prepr_model

--- preprocessing/prepr_model.py
+++ preprocessing/prepr_model.py
@@ -1,19 +1,19 @@
 import torch
-from ..ctm.components import Identity
+import torch.nn as nn
 
 
-def preprocess_backbone(backbone, description='resnet18', dims=7):
+def preprocess_backbone(backbone, description='resnet18', dims=14):
     """Eliminates unused layers from backbone model and removes its parameters
     from the autograd backpropagation graph.
     """
     model = backbone
     if description == 'resnet18':
-        model.avgpool = Identity()
-        model.fc = Identity()
+        model.avgpool = nn.Identity()
+        model.fc = nn.Identity()
 
         if dims == 14:
             # set layer4 to Identity for (14, 14, 256) instead of (7, 7, 512)
-            model.layer4 = Identity()
+            model.layer4 = nn.Identity()
 
     for param in model.parameters():
         param.requires_grad = False
