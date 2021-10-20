00c5ad70c5a3556de61422ef4eec78c7df3b5b7a
Florian Biebel <f.biebeldev@gmail.com>
2021-10-20 10:48:31 +0200

    added PairwiseDistance and some minor changes

--- metric/metric_module.py
+++ metric/metric_module.py
@@ -3,8 +3,7 @@ from torch import nn as nn
 
 class MetricModule(nn.Module):
     """Wrapper module class for a metric learner.
-    Can be a simple metric or a neural network, as long as it works like
-    a nn.Module.
+    Can be a simple metric or a neural network, as long as it inherits from nn.Module.
 
     If your metric is more complicated, adapt the code accordingly.
     """
@@ -17,11 +16,16 @@ class MetricModule(nn.Module):
         return self.metric(x)
 
 
-class CosineModule(MetricModule):
+class CosineSimModule(MetricModule):
     """CosineSimilarity metric."""
     def __init__(self, dim=1):
         super().__init__(nn.CosineSimilarity(dim=dim))
 
+class PairwiseDistModule(MetricModule):
+    """Pairwise Distance metric."""
+    def __init__(self, p=2.0):
+        super().__init__(nn.PairwiseDistance(p))
 
 # contains all implemented 
-METRICS = {'cosine': CosineModule}
+METRICS = {'cosine': CosineSimModule,
+           'pairwise': PairwiseDistModule}
