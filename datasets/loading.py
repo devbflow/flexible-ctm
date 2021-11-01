1de409c08fb5d1c5d3004fac2a60b4f4b089a8b7
Florian Biebel <f.biebeldev@gmail.com>
2021-11-01 18:33:37 +0100

    finished loading for miniImagenet

--- datasets/loading.py
+++ datasets/loading.py
@@ -1,8 +1,11 @@
 import os
 from collections import defaultdict
+import random
+
 import torch
 from torch.utils.data import Dataset, DataLoader, Sampler
 from torchvision.io import read_image
+from torchvision import transforms
 import pandas as pd
 
 
@@ -12,26 +15,56 @@ class MiniImagenetDataset(Dataset):
         super().__init__()
         self.labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+        if transform:
+            self.transform = transform
+        else:
+            size = 224
+            # transform.ToTensor not necessary
+            self.transform = transforms.Compose([transforms.Resize((size,size)),
+                                                 transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        if target_transform:
+            self.target_transform = target_transform
+        else:
+            self.target_transform = None #temporary
 
     def __len__(self):
         return self.labels.shape[0]
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.labels.iloc[idx, 0])
-        img = read_image(img_path)
-        label = self.labels.iloc[idx,1]
-        if self.transform:
-            img = self.transform(img)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return img, label
+        # if we get a list of indices, get several samples
+        if type(idx) == list:
+            imgs = []
+            labels = []
+            for i in idx:
+                img_path = os.path.join(self.img_dir, self.labels.iat[i, 0])
+                img = read_image(img_path).float()
+                label = self.labels.iat[i, 1]
+                if self.transform:
+                    img = self.transform(img)
+                if self.target_transform:
+                    label = self.target_transform(label)
+                imgs.append(img.unsqueeze(dim=0))
+                labels.append(label)
+            imgs_tensor = torch.cat(imgs, dim=0)
+            print("imgs_tensor.shape: ", imgs_tensor.shape)
+            print("len(labels): ", len(labels))
+            return imgs_tensor, labels
+        else:
+            # we only have one sample to load
+            img_path = os.path.join(self.img_dir, self.labels.iloc[idx, 0])
+            img = read_image(img_path).float()
+            label = self.labels.iloc[idx,1]
+            if self.transform:
+                img = self.transform(img)
+            if self.target_transform:
+                label = self.target_transform(label)
+            return img, label
+
 
 class FewShotBatchSampler(Sampler):
 
-    def __init__(self, data_source, targets, n_way, k_shot, include_query=True, shuffle=True):
-        self.dataset = data_source
+    def __init__(self, targets, n_way, k_shot, include_query=True, shuffle=True):
+        #self.dataset = data_source
         self.labels = targets
         self.n = n_way
         self.k = k_shot
@@ -45,20 +78,24 @@ class FewShotBatchSampler(Sampler):
         self.num_classes = len(self.classes)
         self.cls_indices = {}
         self.cls_batches = {}
-        # get indices for each class label and claculate resp. batch number in case of inequalities
+        # get indices for each class label and calculate resp. batch number in case of inequalities
         for c in self.classes:
             self.cls_indices[c] = self.labels.index[self.labels['label'] == c].tolist()
-            self.cls_batches[c] = self.cls_indices[c].shape[0] // self.k
+            self.cls_batches[c] = len(self.cls_indices[c]) // self.k
         self.iterations = sum(self.cls_batches.values()) // self.n
         self.class_list = [c for c in self.classes for _ in range(self.cls_batches[c])]
 
         if self.shuffle:
-            self.labels = self.labels.sample(frac=1)
-            #TODO: shuffle
+            random.shuffle(self.class_list)
+            for c in self.classes:
+                random.shuffle(self.cls_indices[c])
+
+
     def __iter__(self):
         if self.shuffle:
-            #TODO:shuffle
-            pass
+            random.shuffle(self.class_list)
+            for c in self.classes:
+                random.shuffle(self.cls_indices[c])
 
         start_idx = defaultdict(int)
         for i in range(self.iterations):
@@ -66,13 +103,12 @@ class FewShotBatchSampler(Sampler):
             idx_batch = []
             for c in cls_batch:
                 idx_batch.extend(self.cls_indices[c][start_idx[c]:start_idx[c] + self.k])
-                start_idx += self.k
+                start_idx[c] += self.k
 
-            # yield support and query or only one set if include_query
+            print("start_idx: ", start_idx)
+            # yield support and query or only one set if include_query is set accordingly
             if self.include_query:
-                support_set = idx_batch[::2]
-                query_set = idx_batch[1::2]
-                yield support_set, query_set
+                yield idx_batch[::2] + idx_batch[1::2]
             else:
                 yield idx_batch
 
@@ -82,16 +118,17 @@ class FewShotBatchSampler(Sampler):
 DATASETS = {'miniImagenet': MiniImagenetDataset}
 
 def load_dataset(dataset_name, split='train'):
-    """Returns a DataLoader for the specified dataset"""
+    """Returns a Dataset object for the specified dataset"""
     dataset_path = os.path.abspath(dataset_name)
     annotations = os.path.join(dataset_path, split+'.csv')
     img_dir = os.path.join(dataset_path, 'images')
     return DATASETS[dataset_name](annotations, img_dir)
 
 def get_dataloader(dataset_name, n_way, k_shot, include_query=True, shuffle=True, split='train'):
+    """Returs a DataLoader for the specified dataset according to passed args."""
     dataset = load_dataset(dataset_name, split)
     # one batch returned by sampler consists of N*K samples for support and/or query set
-    sampler = FewShotBatchSampler(data_source=dataset,
+    batch_sampler = FewShotBatchSampler(#data_source=dataset,
                                   targets=dataset.labels,
                                   n_way=n_way,
                                   k_shot=k_shot,
@@ -99,20 +136,25 @@ def get_dataloader(dataset_name, n_way, k_shot, include_query=True, shuffle=True
                                   include_query=include_query)
 
     dataloader = DataLoader(dataset=dataset,
-                            sampler=sampler,
+                            batch_sampler=batch_sampler,
                             batch_size=1)
     return dataloader
+
 if __name__ == "__main__":
     loader = get_dataloader(dataset_name='miniImagenet',
-                            n_way=5,
+                            n_way=2,
                             k_shot=5,
                             include_query=True,
                             split='train',
-                            shuffle=False)
+                            shuffle=True)
     # loader check
-    for s, q in loader:
-        print("get_loader test...")
-        print("Support:", s)
-        print("Query:", q)
-        print("...exit get_loader test.")
-        break
+    print("get_loader test...")
+    c = 0
+    for (batch, labels) in loader:
+        if c == 10:
+            break
+        print("Batch:", batch.shape)
+        print("Label:", labels)
+        c += 1
+    print("...exit get_loader test.")
+
