fdf6104262ac62188315bd16358e6a9d017119fa
Florian Biebel <f.biebeldev@gmail.com>
2021-11-03 11:26:03 +0100

    added return of output shape

--- preprocessing/prepr_model.py
+++ preprocessing/prepr_model.py
@@ -14,9 +14,11 @@ def preprocess_backbone(backbone, description='resnet18', dims=14, pretrained=Tr
         if dims == 14:
             # set layer4 to Identity for (14, 14, 256) instead of (7, 7, 512)
             model.layer4 = nn.Identity()
+
     # if pretrained, we don't need trainable params
     if pretrained:
         for param in model.parameters():
             param.requires_grad = False
+    output_shape = model(torch.zeros((1,3,224,224))).view(1, 256, 14, 14).shape
 
-    return model
+    return model, output_shape
