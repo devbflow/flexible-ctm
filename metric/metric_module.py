fe65928e0dc5276bc042f72276f5e7212fc35bef
Florian Biebel <f.biebeldev@gmail.com>
2021-11-08 15:02:48 +0100

    updated PairwiseDistModule

--- metric/metric_module.py
+++ metric/metric_module.py
@@ -27,10 +27,17 @@ class PairwiseDistModule(MetricModule):
         super().__init__(nn.PairwiseDistance(p))
 
     def forward(self, support, query, n_way, k_shot):
+        # conflate channels and dims for support and query set
         support = support.view(support.shape[0], -1)
-        query = query.view(query.shape[0], -1)
-        score = self.metric(support, query)
-        return score.view(query.shape[0], n_way, k_shot)
+        cdd = support.shape[-1]
+        qsize = query.shape[0] # query batch size
+        query = query.view(qsize, -1)
+        # expand support/query to get pairwise distance between all query and support samples
+        exp_supp = support.unsqueeze(0).expand(qsize, -1, -1).contiguous().view(-1, cdd)
+        exp_query = query.unsqueeze(1).expand(-1, n_way*k_shot, -1).contiguous().view(-1, cdd)
+        # get metric score and reshape to (query size, n, k) to sum over k samples
+        score = self.metric(exp_supp, exp_query).view(qsize, n_way, k_shot).sum(dim=2) # (query size, n)
+        return score
 
 # contains all implemented 
 METRICS = {'cosine': CosineSimModule,
@@ -41,7 +48,8 @@ if __name__ == "__main__":
     print("Test PairwiseDistModule...")
     n = 2
     k = 5
-    support = torch.rand((n*k, 2, 3, 4))
-    query = torch.rand((n*k, 2, 3, 4))
+    support = torch.rand((n*k, 3, 4, 4))
+    query = torch.rand((3*n*k, 3, 4, 4))
     pdist = PairwiseDistModule()
     score = pdist(support, query, n, k)
+    print(score)
